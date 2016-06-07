32edb96e542a7409b0eaac9b13f0b6327955910a
kelvinmuchui <muchuikelvin423@gmail.com>
2016-06-07 17:11:51 +0300

    base.html add

--- __init__.py
+++ __init__.py
@@ -6,6 +6,13 @@ app.config["MONGODB_SETTINGS"] = {'DB': "my_tumble_log"}
 app.config["SECRET_KEY"] = "KeepThisS3cr3t"
 
 db = MongoEngine(app)
+def register_blueprints(app):
+    # Prevents circular imports
+    from tumblelog.views import posts
+    app.register_blueprint(posts)
+
+register_blueprints(app)
+
 
 if __name__ ='__main__':
 	app.run(debug = True)
